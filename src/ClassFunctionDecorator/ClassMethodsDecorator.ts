5cfa25bb4e965f886e42243aa3fcab3bf8e6571a
alexchen <AlexChen7022@gmail.com>
2024-06-23 00:42:06 +0800

    Refactor : Add comment

--- src/ClassFunctionDecorator/ClassMethodsDecorator.ts
+++ src/ClassFunctionDecorator/ClassMethodsDecorator.ts
@@ -1,12 +1,27 @@
-
-// Compile target 需要在 ES2017 之後，不然Async的Method 都會是錯誤的
 type Constructor<T = {}> = new (...args: any[]) => T
 
-function isAsyncFunction (method: Function): boolean {
+/**
+ * Determines if a function is asynchronous.
+ * Note: The compile target needs to be ES2017 or later, as only ES2017 formally supports async/await.
+ * Before ES2017, async/await was implemented in a marginal way, so there wouldn't be a constructor named AsyncFunction.
+ *
+ * @param method - The function to check
+ * @returns True if the function is asynchronous, otherwise false
+ */
+function isAsyncFunction(method: Function): boolean {
   return method.constructor.name === 'AsyncFunction'
 }
 
-function wrapMethod (originalMethod: Function, methodName: string, asyncWrapMethod: Function, syncWrapMethod: Function) {
+/**
+ * Wraps a method, choosing the appropriate wrapper based on whether the method is asynchronous.
+ *
+ * @param originalMethod - The original method
+ * @param methodName - The name of the method
+ * @param asyncWrapMethod - The wrapper for asynchronous methods
+ * @param syncWrapMethod - The wrapper for synchronous methods
+ * @returns The wrapped method
+ */
+function wrapMethod(originalMethod: Function, methodName: string, asyncWrapMethod: Function, syncWrapMethod: Function) {
   return function (this: any, ...args: any[]) {
     if (isAsyncFunction(originalMethod)) {
       return asyncWrapMethod(originalMethod, methodName, this, args)
@@ -16,17 +31,28 @@ function wrapMethod (originalMethod: Function, methodName: string, asyncWrapMeth
   }
 }
 
-export function classMethodsDecorator (asyncWrapMethod: Function, syncWrapMethod: Function) {
+/**
+ * Class methods decorator, used to wrap all methods in a class.
+ *
+ * @param asyncWrapMethod - The wrapper for asynchronous methods
+ * @param syncWrapMethod - The wrapper for synchronous methods
+ * @returns The class with wrapped methods
+ */
+export function classMethodsDecorator(asyncWrapMethod: Function, syncWrapMethod: Function) {
   return function <T extends Constructor>(Base: T) {
     return class extends Base {
-      constructor (...args: any[]) {
+      constructor(...args: any[]) {
         super(...args)
 
+        // Get all property names of the class,
+        // Object.keys for arrow function.
+        // Object.getOwnPropertyNames for std class function
         const propertyNames = [
           ...Object.keys(this),
           ...Object.getOwnPropertyNames(Base.prototype)
         ]
 
+        // Check and wrap each method
         propertyNames.forEach(propertyName => {
           if (propertyName === 'constructor') return
           const originalMethod = (this as any)[propertyName]
