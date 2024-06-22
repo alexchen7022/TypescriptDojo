df5ee0e8dbcace2d75f0a5ab6865ceca9ae6f734
alexchen <AlexChen7022@gmail.com>
2024-06-23 00:25:51 +0800

    Refactor : Extract wrapper method

--- src/ClassFunctionDecorator/logClassMethods.ts
+++ src/ClassFunctionDecorator/logClassMethods.ts
@@ -2,61 +2,39 @@
 // Compile target 需要在 ES2017 之後，不然Async的Method 都會是錯誤的
 type Constructor<T = {}> = new (...args: any[]) => T
 
-async function logAsyncMethodExecution(method: Function, methodName: string, context: any, args: any[]) {
-  console.log(`Calling ${methodName} with args: ${JSON.stringify(args)}`);
-  try {
-    const result = await method.apply(context, args);
-    console.log(`Result of ${methodName}: ${result}`);
-    return result;
-  } catch (error) {
-    console.error(`Error in ${methodName}: ${error}`);
-    throw error;
-  }
+function isAsyncFunction (method: Function): boolean {
+  return method.constructor.name === 'AsyncFunction'
 }
 
-function logSyncMethodExecution(method: Function, methodName: string, context: any, args: any[]) {
-  console.log(`Calling ${methodName} with args: ${JSON.stringify(args)}`);
-  try {
-    const result = method.apply(context, args);
-    console.log(`Result of ${methodName}: ${result}`);
-    return result;
-  } catch (error) {
-    console.error(`Error in ${methodName}: ${error}`);
-    throw error;
-  }
-}
-
-function isAsyncFunction(method: Function): boolean {
-  return method.constructor.name === "AsyncFunction";
-}
-
-function wrapMethod (method: Function, methodName: string) {
+function wrapMethod (method: Function, methodName: string, logAsyncMethod: Function, logSyncMethod: Function) {
   return function (this: any, ...args: any[]) {
     if (isAsyncFunction(method)) {
-      return logAsyncMethodExecution(method, methodName, this, args)
+      return logAsyncMethod(method, methodName, this, args)
     } else {
-      return logSyncMethodExecution(method, methodName, this, args)
+      return logSyncMethod(method, methodName, this, args)
     }
   }
 }
 
-export function logClassMethods<T extends Constructor> (Base: T) {
-  return class extends Base {
-    constructor (...args: any[]) {
-      super(...args)
+export function logClassMethods (logAsyncMethod: Function, logSyncMethod: Function) {
+  return function <T extends Constructor>(Base: T) {
+    return class extends Base {
+      constructor (...args: any[]) {
+        super(...args)
 
-      const propertyNames = [
-        ...Object.keys(this),
-        ...Object.getOwnPropertyNames(Base.prototype)
-      ]
+        const propertyNames = [
+          ...Object.keys(this),
+          ...Object.getOwnPropertyNames(Base.prototype)
+        ]
 
-      propertyNames.forEach(propertyName => {
-        if (propertyName === 'constructor') return
-        const originalMethod = (this as any)[propertyName]
-        if (typeof originalMethod === 'function') {
-          (this as any)[propertyName] = wrapMethod(originalMethod, propertyName)
-        }
-      })
+        propertyNames.forEach(propertyName => {
+          if (propertyName === 'constructor') return
+          const originalMethod = (this as any)[propertyName]
+          if (typeof originalMethod === 'function') {
+            (this as any)[propertyName] = wrapMethod(originalMethod, propertyName, logAsyncMethod, logSyncMethod)
+          }
+        })
+      }
     }
   }
 }
