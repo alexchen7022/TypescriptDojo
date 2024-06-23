38c33f44e4c6cc647e1b21a02a7125e291717529
alexchen <AlexChen7022@gmail.com>
2024-06-24 00:50:40 +0800

    Refactor : simplify implementation

--- src/ClassFunctionDecorator/Logger.ts
+++ src/ClassFunctionDecorator/Logger.ts
@@ -1,21 +1,33 @@
 import { Wrapper } from './Wrapper'
 
+// Logger class implementing the Wrapper interface
 class Logger implements Wrapper {
+  // Asynchronous handler method
   asyncHandler = async (method: Function, methodName: string, context: any, args: any[]): Promise<any> => {
-    console.log(`Calling ${methodName} with args: ${JSON.stringify(args)}`)
+    // Log method name and arguments
+    console.log(`Calling ${methodName} with args: ${this.processArgs(args)}`)
+    // Invoke the method with the given context and arguments
     const result: any = await method.apply(context, args)
-    console.log(`Result of ${methodName}: ${this.convertValue(result)}`)
+    // Log the result of the method call
+    console.log(`Result of ${methodName}: ${this.convertResult(result)}`)
+    // Return the result as a resolved Promise
     return await Promise.resolve(result)
   }
 
+  // Synchronous handler method
   syncHandler = (method: Function, methodName: string, context: any, args: any[]): any => {
-    console.log(`Calling ${methodName} with args: ${JSON.stringify(args)}`)
+    // Log method name and arguments
+    console.log(`Calling ${methodName} with args: ${this.processArgs(args)}`)
+    // Invoke the method with the given context and arguments
     const result: any = method.apply(context, args)
-    console.log(`Result of ${methodName}: ${this.convertValue(result)}`)
+    // Log the result of the method call
+    console.log(`Result of ${methodName}: ${this.convertResult(result)}`)
+    // Return the result
     return result
   }
 
-  convertValue = (value: any): string => {
+  // Convert result to string representation
+  convertResult = (value: any): string => {
     if (typeof value === 'string' || typeof value === 'number' || value instanceof Date) {
       return value.toString()
     } else if (Array.isArray(value)) {
@@ -27,18 +39,18 @@ class Logger implements Wrapper {
     }
   }
 
+  // Process object to string representation
   processObject = (obj: { [key: string]: any }): string => {
-    let values: string = ''
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        values = `${values}${key}:${this.convertValue(obj[key])},`
-      }
-    }
-    if (values.length > 1) {
-      values = values.slice(0, -1)
-    }
-
+    const values = Object.keys(obj)
+        .map(key => `${key}:${this.convertResult(obj[key])}`)
+        .join(',')
     return `{${values}}`
+  }
+
+  // Process arguments to string representation
+  processArgs = (args: any[]): string => {
+    const values = args.map(arg => this.convertResult(arg)).join(',')
+    return `[${values}]`
   }
 }
 
